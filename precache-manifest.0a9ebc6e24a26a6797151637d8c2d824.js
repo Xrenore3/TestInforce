0a002e40f29381fbcb0e60df68dc97a40acf2632
Xrenore3 <62048261+Xrenore3@users.noreply.github.com>
2020-11-29 16:02:21 +0200

    Updates

---
+++ precache-manifest.0a9ebc6e24a26a6797151637d8c2d824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed3c473744af999359f15607a3384c0",
+    "url": "/TestInforce/index.html"
+  },
+  {
+    "revision": "9462de2950ef793e94d0",
+    "url": "/TestInforce/static/css/main.e51cfa4d.chunk.css"
+  },
+  {
+    "revision": "4d2929d6f2f187ea3677",
+    "url": "/TestInforce/static/js/2.6b447b45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TestInforce/static/js/2.6b447b45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9462de2950ef793e94d0",
+    "url": "/TestInforce/static/js/main.bb20ce13.chunk.js"
+  },
+  {
+    "revision": "9e7bfe97581acbe2f559",
+    "url": "/TestInforce/static/js/runtime-main.08ff7b59.js"
+  }
+]);
